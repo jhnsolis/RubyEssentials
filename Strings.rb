e126d7342c648c3d393b258ae62788ff569c3bf6
Jonathan Solis <38994980+jhnsolis@users.noreply.github.com>
2019-03-01 19:58:25 -0600

    Add more string examples

--- Strings.rb
+++ Strings.rb
@@ -15,3 +15,21 @@ def multiply(one,two)
     "#{one} multiplied by #{two} equals #{one * two}"
 end
 puts multiply(3,2)
+
+#Using exclamation char (!) will provoke that string methods overwrite the source string
+
+name = ' jon'
+puts name.lstrip.capitalize
+puts name #Variable was not affected
+name.lstrip! #Note the (!) character, this causes the variable to be overwritten
+puts name
+name[0] = 'L' #Replaces first character
+puts name
+
+#%Q Allows multiline strings
+cur_weather = %Q{It's a hot day outside
+                grab your umbrellas}
+cur_weather.lines do |line|
+    line.sub! 'hot', 'rainy' #Replace hot with rainy
+    puts "#{line.strip}" #Remove spaces from the begining of the sentence
+end
