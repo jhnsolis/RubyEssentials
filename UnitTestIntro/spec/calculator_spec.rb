94d34ed521fc492ca59f75ddb449dd4fdbffe2bc
Jonathan <38994980+jhnsolis@users.noreply.github.com>
2019-03-10 23:51:01 -0600

    Add more matcher examples for RSpec

--- UnitTestIntro/spec/calculator_spec.rb
+++ UnitTestIntro/spec/calculator_spec.rb
@@ -1,6 +1,16 @@
 #This file will contain all the specs (aka: tests) for the calculator class
 #Tu run it, go to the main folder /UnitTestIntro and use 'rspec' command
 
+#Use .to or .not_to methods to set the expectations. Those methods take 'matchers'
+#Matcher examples:
+#   eq, be_true, be_false, raise_error(SomeError), be_nil
+#Any predicate (Boolean method) can be a matcher, for example nil's matcher is: be_nil because every object in ruby has a :nil? method
+
+#If you want to know more details about the test results, run it using 'rspec --format documentation' command
+
+#More matcher examples belo
+#https://relishapp.com/rspec/rspec-expectations/docs
+
 require 'rspec'
 require_relative '../Calculator'
 
@@ -18,6 +28,11 @@ describe Calculator do #All specs go into a describe block
 
     it "Should substract two numbers correctly" do
         expect(@calculator.substract(4, 2)).to eq 2
+    end
+
+    it "Should substract two numbers and become even" do
+        expect(@calculator.substract(3, 3)).to be_even
+        expect(@calculator.substract(3, 3)).not_to be_odd
     end
 
     it "Should divide two numbers correctly" do
