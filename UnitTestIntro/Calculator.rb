edefe1ab51c9e676545ecbf8c4cbe5561b714339
Jonathan <38994980+jhnsolis@users.noreply.github.com>
2019-03-10 19:40:33 -0600

    Show a comment for better understanding the scenario

--- UnitTestIntro/Calculator.rb
+++ UnitTestIntro/Calculator.rb
@@ -7,15 +7,15 @@ class Calculator
     end
 
     def add(one, two)
-        one - two
+        one - two #This is wrong this method is supposed to add
     end
 
     def substract(one, two)
-        one + two
+        one + two #This is wrong this method is supposed to substract
     end
 
     def divide(one,two)
         one / two
     end
-    
+
 end
